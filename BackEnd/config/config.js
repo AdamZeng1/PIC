eaffe1e319bc7d7c90b2d1fe54205bbea64873e9
adamzeng <khklara3@gmail.com>
2019-08-28 12:01:35 +0800

    dockerfile_docker_compose_dockerignore

--- BackEnd/config/config.js
+++ BackEnd/config/config.js
@@ -13,13 +13,23 @@ module.exports = {
     'apiKey': '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',
 
     // MongoDB的地址
-    'database': 'mongodb://localhost:27017/test',
+    'mongo_host': process.env.MONGO_HOST || 'localhost',
+    'mongo_port': process.env.MONGO_PORT || 27017,
+    'mongo_database': process.env.MONGO_DB || 'test',
+
+    // Redis address
+    'redis_host': process.env.REDIS_HOST||'localhost',
+    'redis_port': process.env.REDIS_PORT||'6379',
+    'redis_password': process.env.REDIS_PASSWORD||'foobared',
+
+    // port
+    'port': process.env.PORT,
 
     // 邮箱验证成功之后的重定向地址
-    'redirectUrl': 'http://localhost:4000/login',
+    'redirectUrl': 'http://localhost:3000/login',
 
     // 网站路径
-    'url': 'http://localhost:4000'
+    'url': 'http://localhost:3000'
 };
 
 
