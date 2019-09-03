053051f3ef354ccedf2b3107ff9f4f94eecf080a
JohnZhang <johnzhangzzm@gmail.com>
2019-09-03 17:01:55 +1000

    Newpost compoment can set request and relevant data to backend after upload a image, but got an 401 error.

--- FrontEnd/frontend/src/Components/NewPost/NewPost.js
+++ FrontEnd/frontend/src/Components/NewPost/NewPost.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
-import { Icon, Modal, Tooltip} from 'antd';
+import { Icon, Modal, Tooltip, Button} from 'antd';
 import UploadImg from '../Upload/Upload';
 import axios from 'axios';
+import classes from './NewPost.module.css';
 
 var qiniu = require('qiniu-js');
 
@@ -10,30 +11,18 @@ var config = {
   region: qiniu.region.as0 // service area
 };
 
-const observer = {
-  next(res){
-    // ...
-  },
-  error(err){
-    // ...
-    console.log(err);
-  },
-  complete(res){
-    console.log("upload succeeded");
-    console.log('result', res);
-  }
-}
-
-let putExtra = {
-  fname: "",
-  params: {},
-  mimeType: ["image/png", "image/jpeg", "image/gif"]
-};
-
-
 class NewPost extends Component {
 
-  state = { visible: false };
+  state = { 
+    visible: false,
+    postTittle: null,
+    putExtra : {
+      fname: "",
+      params: {},
+      mimeType: ["image/png", "image/jpeg", "image/gif"]
+    },
+
+   };
 
   showModal = () => {
     this.setState({
@@ -57,14 +46,55 @@ class NewPost extends Component {
 
   submit = async (e) => {
     e.preventDefault();
+    const postTittle = this.state.postTittle;
+    const observer = {
+      next(res){
+        // ...
+      },
+      error(err){
+        // ...
+        console.log(err);
+      },
+      complete(res){
+        console.log("upload succeeded");
+        console.log('result', res);
+        axios.post("http://localhost:9000/post/",
+          {
+            headers: {
+            'Authorization': localStorage.Token,
+            },
+            params: {
+              "title": postTittle,
+              "image_url": "pw4a0goy8.sabkt.gdipper.com/" + res.name,
+              "topic": "test default"
+            }
+          }).then((response) => {
+            console.log(response);
+          }).catch(error => {
+            console.error(error);
+          })
+      }
+    }
     let formData = new FormData(e.target);
-    const fileName = formData.get('file').name;
+    const putExtra = {...this.state.putExtra}
+    const fileName = this.state.postTittle;
+    console.log(putExtra.fname);
     const file = formData.get('file');
     const result = await axios.get("http://localhost:9000/qiniu/token");
     const qiniuToken = result.data['qiniu-token'];
     const observable = qiniu.upload(file, fileName, qiniuToken, putExtra, config)
     var subscription = observable.subscribe(observer) // 上传开始
+    this.handleOk();
   };
+
+  inputChangeHandler = (e) => {
+    let newInput = e.target.value;
+    const newExtra = {...this.state.putExtra}
+    newExtra.fname = newInput;
+    this.setState({
+        putExtra: newExtra,
+        postTittle: e.target.value})
+  }
 
   render(){
     return(
@@ -73,16 +103,27 @@ class NewPost extends Component {
           <Icon type="edit" onClick={this.showModal}/>
         </Tooltip>
         <Modal
-          title="Basic Modal"
+          centered
+          title="Create a new post"
           visible={this.state.visible}
           onOk={this.handleOk}
           onCancel={this.handleCancel}
+          footer={null}
         >
          {/* <UploadImg /> */}
-         <form onSubmit={this.submit}>
-          <input type="file" name='file'/>
-          <input type="submit" value="Upload"/>
-        </form>
+         <form onSubmit={this.submit} className={classes.FormElement}>
+            <div >
+              <label>Tittle: </label>
+              <input type="text" name='tittle' onChange={(e)=>this.inputChangeHandler(e)}/>
+            </div>
+            <div >
+              <label>File: </label>
+              <input type="file" name='file'/>
+            </div>
+            <div>
+              <Button type='primary' htmlType='submit'>Submit</Button>
+            </div>   
+          </form>
         </Modal>
       </div>
     )
