9e335caf1f8f3ecf9bbc6f5d22214770aba37682
JohnZhang <johnzhangzzm@gmail.com>
2019-09-21 10:50:28 +1000

    Add pagination to postlist

--- FrontEnd/frontend/src/Containner/PostList/PostList.js
+++ FrontEnd/frontend/src/Containner/PostList/PostList.js
@@ -2,28 +2,39 @@ import React, {Component} from 'react';
 import {withRouter} from 'react-router-dom';
 import PostItem from '../../Components/Post/PostItem';
 import classes from './PostList.module.css';
-import {Spin, Row} from 'antd';
+import {Spin, Row, Pagination} from 'antd';
 import axios from '../../axios-pic';
 
 class PostList extends Component {
 
   state={
     posts: null,
-    postsNum: {
-      page: 1,
-      per_page: 4,
-    },
+    page: 1,
+    per_page: 2,
     loading: true,
   }
 
   componentWillMount(){
-    const query = 'page=' + this.state.postsNum.page + '&' + 'per_page=' + this.state.postsNum.per_page;
+    const query = 'page=' + this.state.page + '&' + 'per_page=' + this.state.per_page;
     axios.get('/post?'+query)
       .then(res => {
         console.log(res);
         this.setState({posts: res.data.posts, loading: false})
       })
       .catch(err => console.log(err));
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if (prevState.page !== this.state.page){
+      const query = 'page=' + this.state.page + '&' + 'per_page=' + this.state.per_page;
+    axios.get('/post?'+query)
+      .then(res => {
+        console.log(res);
+        this.setState({posts: res.data.posts, loading: false})
+        document.documentElement.scrollTop=0;
+      })
+      .catch(err => console.log(err));
+    }
   }
 
   postClickHandler = (postData) => {
@@ -34,6 +45,10 @@ class PostList extends Component {
     this.props.history.push(path);
   }
 
+  pageOnChangeHandler = (pageNum) => {
+    this.setState({page: pageNum}, ()=>{console.log(this.state.page)});
+    // this.forceUpdate();
+  }
   render(){
     let posts = <Spin size="large" />;
     if (this.state.posts){
@@ -50,8 +65,9 @@ class PostList extends Component {
     }
     return(
       <div className={classes.PostList}>
-      {posts}
-      {/* <PostItem imgURL='https://images.unsplash.com/photo-1565847179096-9af38a6af8a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80' /> */}
+        {posts}
+        {/* <PostItem imgURL='https://images.unsplash.com/photo-1565847179096-9af38a6af8a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80' /> */}
+        <Pagination total={500} onChange={this.pageOnChangeHandler} />
       </div>
     );
   }
