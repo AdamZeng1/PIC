e70a2e20a5c25ded08a6879ac5664cc0a860a7a0
adamzeng <khklara3@gmail.com>
2019-10-03 09:21:47 +0800

    deploy-app-fix

--- server/config/config.js
+++ server/config/config.js
@@ -26,8 +26,8 @@ module.exports = {
     'port': process.env.PORT || 5000,
 
     // 邮箱验证成功之后的重定向地址
-    'redirectUrl': 'http://'+process.env.API_HOST+":"+process.env.APP_PORT+"/api/validateSuccessful.html",
+    'redirectUrl': 'http://'+process.env.APP_HOST+":"+process.env.APP_PORT+"/api/validateSuccessful.html",
 
     // 网站路径
-    'url': 'http://'+process.env.API_HOST+":"+process.env.APP_PORT+"/api"
+    'url': 'http://'+process.env.APP_HOST+":"+process.env.APP_PORT+"/api"
 };
